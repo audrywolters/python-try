19dd185fb5167bf693fece8c17f3fb01fb565ae8
Audry Wolters <audrywolters@gmail.com>
2020-08-18 14:55:20 -0500

    array loop object

--- hello.py
+++ hello.py
@@ -1,6 +1,25 @@
 
+# function and variable
 def helloFriend( friendName ):
     print ( 'hello ' + friendName )
 
 friends = 'Jill and Megan'
 helloFriend( friends )
+
+# array and loop
+coolArray = [ 'thing', 2, True ]
+
+for item in coolArray:
+    print( item )
+
+# object
+class Cat:
+    def __init__( self, name, color ):
+        self.name = name
+        self.color = color
+
+kitty1 = Cat( 'Mina', 'brown spotted tabby' )
+kitty2 = Cat( 'Mervyn', 'black' )
+
+print( 'Names: ' + kitty1.name, kitty2.name )
+print( 'Colors: ' + kitty1.color, kitty2.color )
