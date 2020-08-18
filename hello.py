ae204f273202edf3a0c414dcb0e2495c1d5a890d
Audry Wolters <audrywolters@gmail.com>
2020-08-18 15:11:44 -0500

    operators

--- hello.py
+++ hello.py
@@ -6,11 +6,13 @@ def helloFriend( friendName ):
 friends = 'Jill and Megan'
 helloFriend( friends )
 
+
 # array and loop
 coolArray = [ 'thing', 2, True ]
 
 for item in coolArray:
     print( item )
+
 
 # object
 class Cat:
@@ -23,3 +25,37 @@ kitty2 = Cat( 'Mervyn', 'black' )
 
 print( 'Names: ' + kitty1.name, kitty2.name )
 print( 'Colors: ' + kitty1.color, kitty2.color )
+
+
+# return value function
+def sumNumbers( num1, num2 ):
+    return num1 + num2
+
+print( '1 + 2 =', sumNumbers( 1, 2 ) )
+
+
+# conditionals
+def equalOperator(): 
+    if ( 1 == 2 ):
+        print ( 'equal: True' )
+    else:
+        print ( 'equal: False' )
+
+equalOperator()
+
+
+one = 1
+two = 2
+def andOperator():
+    if ( one == 1 and two == 2 ):
+        print( 'and: True' )
+    else:
+        print( 'and: False' )
+
+andOperator()
+
+def orOperator():
+    if ( one == 2 or two == 2 ):
+        print( 'or: True' )
+
+orOperator()
